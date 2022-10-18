1c85c9c2c41e538af6b20a4657727723d2da3e92
MarcosRodio <81511614+MarcosRodio@users.noreply.github.com>
2022-10-18 18:30:17 -0300

    Atualização

---
+++ Cusro_Python_Pandas_Digital_Innovation-master/ETL.py
@@ -0,0 +1,7 @@
+#ETL -> extract, transform and load
+
+#Extract -> extrair dados de diversas fontes e formatos
+#transform -> linhas colunas calculadas, tipos de dados, mesclar, tabelas
+#load -> carregar para o modelo de dados ex. para um data wharehouse
+
+#ETL é a formatação e padronização do modelo de dados, se não ha manipulação e padronização, não ha etl
